220d1534aec50f59286adeaa362940bc30c931e9
MarioLFB <popesco19@hotmail.com>
2024-03-31 17:16:46 +0100

    Refactor weather and temperature functions

--- weather.py
+++ weather.py
@@ -8,42 +8,38 @@ def options():
     print("3 - Exit")
     option = input("Choose an option: ")
     if option == "1":
-        weather()
+        city = input("Enter the city: ")
+        weather(get_weather_data(city))
     elif option == "2":
-        temperature()
+        city = input("Enter the city: ")
+        temperature(get_weather_data(city))
     elif option == "3":
         exit()
     else:
         print("Invalid option")
         options()
 
-def weather():
-    city = input("Enter the city: ")
+def get_weather_data(city):
     link = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}"
     requisicao = requests.get(link)
     if requisicao.status_code != 200:
         print("City not found")
-        temperature()
-        return
-    data = requisicao.json()
+        return None
+    return requisicao.json()
+
+def weather(data):
+    city = data['name']
     print(f"The sky in {city} is {data['weather'][0]['description']}")
 
-def temperature():
-    city = input("Enter the city: ")
-    link = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}"
-    requisicao = requests.get(link)
-    if requisicao.status_code != 200:
-        print("City not found")
-        temperature()
-        return
-    data = requisicao.json()
+def temperature(data):
+    city = data['name']
     print(f"The temperature in {city} is {data['main']['temp'] - 273.15:.0f}°C")
     print(f"The feels Like in {city} is {data['main']['feels_like'] - 273.15:.0f}°C")
-
 
 
 def main():
     print("Welcome to the weather system!")
     options()
+
 main()
 
