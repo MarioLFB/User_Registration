369f141cd2fe77c1f0b4e4b6e4f65b11bb0aee51
MarioLFB <popesco19@hotmail.com>
2024-04-02 19:03:07 +0100

    Refactor user registration and login functions

--- run.py
+++ run.py
@@ -5,41 +5,42 @@ import getpass
 def cadastro_usuario():
     """
     Function to register a user in the system and save it in the file
-    "usuarios.json" with the name and password of the user.  
+    usuarios.json with the name and password of the user.
     """
-    senha = "" 
-    
-    while True: 
+    senha = ""
+
+    while True:
         name = input("How you want to be called?\n")
 
         try:
-            with open('usuarios.json', 'r') as arquivo: 
-                usuarios = json.load(arquivo) 
-        except json.decoder.JSONDecodeError: 
-            usuarios = [] 
+            with open('usuarios.json', 'r') as arquivo:
+                usuarios = json.load(arquivo)
+        except json.decoder.JSONDecodeError:
+            usuarios = []
 
-        name_registered = False 
-        for user in usuarios: 
-            if user["name"] == name: 
-                print("Name already registered") 
-                name_registered = True 
+        name_registered = False
+        for user in usuarios:
+            if user["name"] == name:
+                print("Name already registered")
+                name_registered = True
                 break
-        
+
         if not name_registered:
-            senha = getpass.getpass("Choose a password with 6 digits:\n") 
-            if name == senha or name == "" or len(senha) != 6 or senha.strip() == "":  
+            senha = getpass.getpass("Choose a password with 6 digits:\n")
+            if name == senha or name == "" or len(senha) != 6 or senha.strip() == "":
                 print("Invalid name or password")
             else:
                 print(f"Thank you {name}! Registration successfully Completed!")
-                usuarios.append({"name": name, "password": senha}) 
-                with open('usuarios.json', 'w') as arquivo: 
-                    json.dump(usuarios, arquivo) 
+                usuarios.append({"name": name, "password": senha})
+                with open('usuarios.json', 'w') as arquivo:
+                    json.dump(usuarios, arquivo)
                 return_menu()
                 break
 
+
 def return_menu():
     """
-    Function to return to the main menu, login or exit the system.    
+    Function to return to the main menu, login or exit the system.
     """
     print("1 - Return to the menu")
     print("2 - Login")
@@ -55,31 +56,33 @@ def return_menu():
         print("Invalid option")
         return_menu()
 
+
 def login():
     """
-    Function to login the user in the system and check if the name and password are correct.        
+    Function to login the user in the system and check if the name and password are correct.
     """
-    with open('usuarios.json', 'r') as arquivo: 
-        try: 
-            usuario = json.load(arquivo) 
-        except json.decoder.JSONDecodeError: 
-            print("No user registered yet") 
-            return 
-    name = input("Enter your name: ") 
-    password = getpass.getpass("Enter your password: ") 
-    for user in usuario: 
-        if user["name"] == name and user["password"] == password: 
-            print("Login successfully completed!") 
+    with open('usuarios.json', 'r') as arquivo:
+        try:
+            usuario = json.load(arquivo)
+        except json.decoder.JSONDecodeError:
+            print("No user registered yet")
+            return
+    name = input("Enter your name: ")
+    password = getpass.getpass("Enter your password: ")
+    for user in usuario:
+        if user["name"] == name and user["password"] == password:
+            print("Login successfully completed!")
             import weather
             weather.main()
-            return 
-    print("Invalid name or password") 
-    login() 
+            return
+    print("Invalid name or password")
+    login()
 
 
 def main():
     """
-    Function to show the main menu of the system and call the functions to register, login or exit the system.      
+    Function to show the main menu of the system and call the functions to register,
+    login or exit the system.
     """
     print("Welcome to Real-Time Weather Forecast")
     print("1 - Register")
@@ -96,5 +99,6 @@ def main():
     else:
         print("Invalid option")
         main()
+
 
 main()
